d4fba2be28f7263d4c9ac97b16e961450ffaa1b7
Christian Kurz <chrissikurz@gmail.com>
2018-08-11 14:25:25 +0100

    Revert "reorder includes / exports"
    This reverts commit 552fa16d2ff9b9d79f85f2c126e96578cfa95d82.

--- src/REPLGamesBase.jl
+++ src/REPLGamesBase.jl
@@ -1,16 +1,15 @@
 module REPLGamesBase
 
 using Crayons, REPL
-
-include("rawmode.jl")
-include("cursor.jl")
-include("readkey.jl")
-
 export rawmode, clear_screen, readKey, put, terminal_screen
 
 function __init__()
     global terminal
     terminal = REPL.Terminals.TTYTerminal(get(ENV, "TERM", Base.Sys.iswindows() ? "" : "dumb"), stdin, stdout, stderr)
 end
+
+include("rawmode.jl")
+include("cursor.jl")
+include("readkey.jl")
 
 end # module
